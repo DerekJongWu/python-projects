3971b5a2454d037d45d900baa21d1181cdb65084
Derek Wu <derekwu@10-17-106-212.dynapool.wireless.nyu.edu>
2023-10-11 18:21:28 -0400

    Add docstrings to all the methods

--- bagels/bagels.py
+++ bagels/bagels.py
@@ -2,6 +2,10 @@
 import random
 
 def start():
+    """
+    This method gives the instructions to the player.
+    
+    """
     print("""Welcome to the Bagels Game 
           I am thinking of a 3-digit number. Try to guess what it is
           Here are some clues: 
@@ -16,6 +20,9 @@ def start():
           )
 
 def init_truth_value() -> str: 
+    """ 
+    Generate a random 3 digit number for the players to guess
+    """
     n = 3
     num = random.randrange(1, 10**3)
     # using format
@@ -25,6 +32,13 @@ def init_truth_value() -> str:
     return num_with_zeros
 
 def validate_guess(truth_digits, guess_digits):
+    """
+    Take a guess from the user and compare it to the truth value. Print out the clues. 
+
+    Args: 
+        truth_digits (List[str]): A list of all the digits of the truth value in string form
+        guess_digits (List[str]): A list of all the digits of the player's guess in string form 
+    """
     count = 0
     for i,digit in enumerate(guess_digits):
         if truth_digits[i] == digit: 
@@ -39,6 +53,12 @@ def validate_guess(truth_digits, guess_digits):
         print("Bagels")
 
 def gameplay(truth): 
+    """" 
+    Run through the actual gameplay of Bagels. Gives the player 10 tries to guess the truth value 
+
+    Args: 
+        truth(int) : the truth value the player is trying to guess
+    """
     num_tries = 1
     truth_digits = [*truth]
     while num_tries < 11:
@@ -57,12 +77,19 @@ def gameplay(truth):
     return
 
 def game(): 
+    """
+    Run through all of the steps of the game from the initial prompt to the actual gameplay
+    """
     start()
     truth = init_truth_value() 
     gameplay(truth)
     replay()
 
 def replay(): 
+    """ 
+    Ask the user if they would like to replay the game, if yes then start the game over again, 
+    if no then exit. 
+    """
     cont = input("Do you want to play again? (yes or no): ")
     if cont == "yes":
         game()
